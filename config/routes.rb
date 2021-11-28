c932d7c67092cdb9c94784b02c72e8da8238d1b2
Lucas Schell <lucas.schell@acad.pucrs.br>
2021-11-28 05:14:51 -0300

    add bootstrap, home page and nav-bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'main#index'
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
