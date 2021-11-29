a36d9b4aa05576bf1bba2b4cb089539b80d41a91
Lucas Schell <lucas.schell@acad.pucrs.br>
2021-11-29 04:35:54 -0300

    feat: error messages in sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   has_secure_password
   has_many :books
 
+  validates_presence_of :name, message: 'O nome é obrigatório.'
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'O email não é válido.' }
+  validates_length_of :password, in: 6..30, message: 'A senha deve ter pelo menos 6 caracteres.'
+  validate :check_password_confirmation
+  def check_password_confirmation
+    errors.add(:password_confirmation, 'As senhas devem ser iguais.') if :password != :password_confirmation
+  end
 end
