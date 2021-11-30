c0b76c7cc51aa0c00aef6a25d34459d1db5c565e
Lucas Schell <lucas.schell@acad.pucrs.br>
2021-11-30 20:15:48 -0300

    feat: pagination with kaminari
    pagination added in "home" and "my books" pages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,19 @@
 class MainController < ApplicationController
   def index
-    @books = Book.all.order(created_at: :desc)
+    reuslts_per_page = 5
+    param_page = params[:page].nil? || params[:page].to_i.zero? ? 1 : Integer(params[:page])
+    total_pages = Book.page(1).per(reuslts_per_page).total_pages
+    page = Book.page(param_page).per(reuslts_per_page).out_of_range? ? total_pages : param_page
+    initial_pagination = if page < 5 || total_pages < 11
+                           1
+                         elsif total_pages - page < 6
+                           total_pages - 9
+                         else
+                           page - 4
+                         end
+
+    @books = Book.order(created_at: :desc).page(page).per(reuslts_per_page)
+    @info = { page: page, total_pages: total_pages,
+              initial_pagination: initial_pagination }
   end
 end
